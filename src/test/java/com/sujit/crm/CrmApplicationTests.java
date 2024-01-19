192d0c4e0392436fe8ada7666b522ad2ec868006
Sujit Jayaraj <35429693+sujitjayaraj@users.noreply.github.com>
2024-01-19 21:22:54 +0530

    Initial Commit

---
+++ src/test/java/com/sujit/crm/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujit.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
