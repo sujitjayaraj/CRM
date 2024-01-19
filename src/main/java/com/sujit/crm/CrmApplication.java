166bb00b34477c27716304c5a84a072e2f8a886b
Sujit Jayaraj <35429693+sujitjayaraj@users.noreply.github.com>
2024-01-19 21:43:01 +0530

    Implemented the main CrmApplication class

--- src/main/java/com/sujit/crm/CrmApplication.java
+++ src/main/java/com/sujit/crm/CrmApplication.java
@@ -1,13 +1,53 @@
 package com.sujit.crm;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
-public class CrmApplication {
+public class CrmApplication implements AsyncConfigurer, SchedulingConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(CrmApplication.class, args);
     }
 
+    /**
+     * @return ThreadPoolTaskExecutor
+     */
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setMaxPoolSize(10);
+        executor.setThreadNamePrefix("TaskExecutor-");
+        executor.initialize();
+        return executor;
+    }
+
+    /**
+     * @param taskRegistrar ScheduledTaskRegistrar
+     */
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("ScheduledTaskExecutor-");
+        scheduler.initialize();
+        taskRegistrar.setTaskScheduler(scheduler);
+    }
+
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }
 }
