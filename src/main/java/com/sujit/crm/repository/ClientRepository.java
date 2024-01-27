8cc17c93ed157539baf2602de236c5ab115c9ef5
Sujit Jayaraj <35429693+sujitjayaraj@users.noreply.github.com>
2024-01-27 14:16:53 +0530

    Client service and its implementation
    interface ClientService and class ClientServiceImplementation along with ClientRepository and ContactPersonRepository

---
+++ src/main/java/com/sujit/crm/repository/ClientRepository.java
@@ -0,0 +1,28 @@
+package com.sujit.crm.repository;
+
+import com.sujit.crm.entity.Client;
+import com.sujit.crm.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    Client findByName(String name);
+    List<Client> findByNameOrderByNameAsc(String name);
+    List<Client> findByNameAndAddressCityOrderByNameAsc(String name, String city);
+
+    List<Client> findByUser(User user);
+    List<Client> findByUserOrderByNameAsc(User user);
+    List<Client> findByUserIn(List<User> users);
+    List<Client> findByCreatedAfter(LocalDate date);
+    List<Client> findByCreatedBetween(LocalDate startDate, LocalDate endDate);
+    List<Client> findByStatusOrderByNameAsc(String status);
+    List<Client> findByStatusAndAddressCityOrderByNameAsc(String status, String city);
+    List<Client> findByAddressCityOrderByNameAsc(String city);
+    List<Client> findByAddressStateOrderByNameAsc(String state);
+    List<Client> findByTin(String tin);
+}
---
+++ src/main/java/com/sujit/crm/service/ClientServiceImplementation.java
@@ -0,0 +1,65 @@
+package com.sujit.crm.service;
+
+import com.sujit.crm.AuthenticationFacade;
+import com.sujit.crm.entity.Client;
+import com.sujit.crm.entity.User;
+import com.sujit.crm.repository.AddressRepository;
+import com.sujit.crm.repository.ClientRepository;
+import com.sujit.crm.repository.ContactPersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Arrays;
+
+public class ClientServiceImplementation implements ClientService{
+
+    @Autowired
+    private AuthenticationFacade authenticationFacade;
+    private final ClientRepository clientRepository;
+    private final ContactPersonRepository contactPersonRepository;
+    private final AddressRepository addressRepository;
+    public ClientServiceImplementation(ClientRepository clientRepository, ContactPersonRepository contactPersonRepository, AddressRepository addressRepository) {
+        this.clientRepository = clientRepository;
+        this.contactPersonRepository = contactPersonRepository;
+        this.addressRepository = addressRepository;
+    }
+    @Override
+    public Client findById(Long id) {
+        return clientRepository.findOne(id);
+    }
+    @Override
+    public Client findByName(String name) {
+        return clientRepository.findByName(name);
+    }
+    @Override
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+    @Override
+    public List<String> getStatusList() {
+        return Arrays.asList("lead", "client", "lost");
+    }
+    @Override
+    public Long saveClientWithLoggedUser(Client client) {
+        User user = authenticationFacade.getAuthenticatedUser();
+        client.setUser(user);
+        client.setCreatedOn(LocalDateTime.now());
+        clientRepository.save(client);
+        return client.getId();
+    }
+    @Override
+    public Long saveClient(Client client) {
+        client.setCreatedOn(LocalDateTime.now());
+        clientRepository.save(client);
+        return client.getId();
+    }
+    @Override
+    public void deleteClient(Long id) {
+        clientRepository.delete(id);
+    }
+    @Override
+    public List<Client> findByUser(User user) {
+        return clientRepository.findByUser(user);
+    }
+}
